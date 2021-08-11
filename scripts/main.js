d3fdda310bc0ffbe8e9b7f5738aebe71948dc91d
AndreUdotai <ubongudotai@gmail.com>
2021-08-11 21:29:03 +0100

    general improvements

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,44 @@
-const longitudeCoordinates 		= document.getElementById('longitudeCoordinates');
-const latitudeCoordinates 		= document.getElementById('latitudeCoordinates');
-const searchForm 				= document.getElementById('searchText');
-const mapDisplay				= document.getElementById('map');
+const searchFormLandingPage 	= document.getElementById('searchFormLandingPage');
+const searchFormHomePage 		= document.getElementById('searchFormHomePage');
+const searchInputLandingPage	= document.getElementById('searchInputLandingPage');
+const searchInputHomePage		= document.getElementById('searchInputHomePage');
+const mapDisplay				= document.getElementById('mapDisplay');
 
-let apiRequest = new XMLHttpRequest();
-// let mapRequest = new XMLHttpRequest();
-let baseUrl	= 'https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&searchtext=';
+let cityLabel		= document.getElementById('cityLabel');
+let weatherCondtion	= document.getElementById('weatherCondition');
+let postalCode		= document.getElementById('postalCode');
+let temperature		= document.getElementById('temperature');
+let humidity		= document.getElementById('humidity');
+let windSpeed		= document.getElementById('windSpeed');
+let longitude		= document.getElementById('longitude');
+let latitude		= document.getElementById('latitude');
+let celsius			= document.getElementById('celsius');
+let fahrenheit		= document.getElementById('fahrenheit');
+let landmarkName 	= document.getElementById('landmarkName');
+let temperatureControls = document.getElementById('temperatureControls');
 
-// let mapUrl = 'https://www.mapquestapi.com/staticmap/v5/map?key=8Dki0pvPAIdaPvLx8D7svwxs72IUthCN&center='
 
-// let mapUrl = 'https://www.mapquestapi.com/staticmap/v5/map?key=8Dki0pvPAIdaPvLx8D7svwxs72IUthCN&center=5.02092,7.93444&size=@2x'
 
-searchForm.addEventListener('submit', ($event) => {
+let apiRequest 			= new XMLHttpRequest();
+let weatherApiRequest 	= new XMLHttpRequest();
+
+let hereApiBaseUrl	= 'https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&searchtext=';
+
+
+searchFormHomePage.addEventListener('submit', ($event) => {
 	$event.preventDefault();
-	
-	const city = document.getElementById('searchTextInput').value;
-	const url = baseUrl + city
+
+	temperatureControls.style.display = 'inline';
+
+	const chosenCity = searchInputHomePage.value;
+	const url = hereApiBaseUrl + chosenCity + '&mode=retrieveLandmarks&prox=6.4584,7.5464,1000';
 	apiRequest.open('GET', url);
 	apiRequest.send();
-});
 
-// searchForm.addEventListener('submit', ($event) => {
-// 	$event.preventDefault();
-	
-// 	const city = document.getElementById('searchTextInput').value;
-// 	const url = mapUrl + city
-// 	apiRequest.open('GET', url);
-// 	apiRequest.send();
-// });
+	weatherApiRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e&units=metric');
+	weatherApiRequest.send();
+
+});
 
 
 apiRequest.onreadystatechange = () => {
@@ -35,74 +46,107 @@ apiRequest.onreadystatechange = () => {
 		if(apiRequest.status === 404) {
 			return message.textContent = 'City not found!';
 		}
+
 		const response = JSON.parse(apiRequest.response);
 
-		let longitudeCoordinate = response.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
-		let latitudeCoordinate 	= response.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+		let longitudeCoordinate 	= response.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
+		let latitudeCoordinate 		= response.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
 
-// 		longitudeCoordinates.textContent 	= longitudeCoordinate;
-// 		latitudeCoordinates.textContent 	= latitudeCoordinate;
+		cityLabel.innerText 		= response.Response.View[0].Result[0].Location.Address.Label;
+		let postCode				= response.Response.View[0].Result[0].Location.Address.PostalCode;
 
-	L.mapquest.key = '8Dki0pvPAIdaPvLx8D7svwxs72IUthCN';
 
-    var map = L.mapquest.map('map', {
-       center: 	[latitudeCoordinate, longitudeCoordinate],
-       layers: 	L.mapquest.tileLayer('map'),
-       zoom: 	12,
-       type: 'dark',
-    });
+		if(postCode != undefined){
+			postalCode.textContent	= 'Postal code: ' + postCode;
+		} else {
+			postalCode.textContent	= 'Not Available.'
+		}
+		longitude.innerText			= 'Longitude: ' + longitudeCoordinate;
+		latitude.innerText			= 'Latitude: ' + latitudeCoordinate + ' | ';
 
-    map.addControl(L.mapquest.control());
+		map.remove();
+
+		buildMap(latitudeCoordinate, longitudeCoordinate);
+
+		landmarkRetrieve(latitudeCoordinate, longitudeCoordinate);
+
 	};
 };
 
-// apiRequest.onreadystatechange = () => {
-// 	if(apiRequest.readyState === 4) {
-// 		if(apiRequest.status === 404) {
-// 			return message.textContent = 'City not found!';
-// 		}
-// 		const response = apiRequest.response;
-// 		console.log(response);
-		// mapDisplay.textContent = response;
- 		// reportSection.textContent = 'The weather in ' + response.name + ' is ' + response.weather[0].main + '.';
+// function for building the mapquest web SDK
+function buildMap(latitude, longitude){
+	mapDisplay.innerHTML = "<div id='map' style='width: 100%; height: 500px;'></div>";
+	L.mapquest.key = '8Dki0pvPAIdaPvLx8D7svwxs72IUthCN';
+	
+	let map = new L.mapquest.map('map', {
+		center: 	[latitude, longitude],
+		layers: 	L.mapquest.tileLayer('map'),
+		zoom: 		12,
+		type: 		'dark',
+	});
 
+	map.addControl(L.mapquest.control());
+}
 
-		// let longitudeCoordinate = response.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
-		// let latitudeCoordinate 	= response.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+// function for temperature conversion between celsius and fahrenheit
+function convertToF(celsius) {
+	let fahrenheit;
+   	fahrenheit = celsius * 9/5 + 32;
+	return fahrenheit.toFixed(2);
+}
 
-		// longitudeCoordinates.textContent 	= longitudeCoordinate;
-		// latitudeCoordinates.textContent 	= latitudeCoordinate;
+weatherApiRequest.onreadystatechange = () => {
+	if(weatherApiRequest.readyState === 4) {
+		if(weatherApiRequest.status === 404) {
+			return message.textContent = 'City not found!';
+		}
+		const response = JSON.parse(weatherApiRequest.response);
 
+                            
+		weatherCondtion.innerHTML	= '<span><i class="fa fa-cloud fa-3x" aria-hidden="true"></i> ' + response.weather[0].description  + '</span>';
+		temperature.innerHTML		= '<span>' + response.main.temp +  '</span>';
+		humidity.textContent		= 'Humidity: ' + response.main.humidity + '%';
+		windSpeed.textContent		= 'Wind: ' + response.wind.speed + 'm/s';
 
+		fahrenheit.addEventListener('click', ($event) => {
+			$event.preventDefault();
+			temperature.textContent = convertToF(response.main.temp);
+		})
 
-		// convert to Base64
-		// let b64Response = btoa(response);
-		// let b64Response = btoa(unescape(encodeURIComponent(response)))
-		// // create an image
-		// let outputImg = document.createElement('img');
-		// outputImg.src = 'data:image/png;base64,'+b64Response;
+		celsius.addEventListener('click', ($event) => {
+			$event.preventDefault();
+			temperature.textContent = response.main.temp;
+		})
+	};
+}
 
-		// // append it to your page
-		// mapDisplay.appendChild(outputImg);
+function landmarkRetrieve(lat, long){
+	let landmarkRequest		= new XMLHttpRequest();
+	landmarkRequest.open('GET', 'https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&mode=retrieveLandmarks&prox=' + lat + ',' + long + ',1000');
 
+	landmarkRequest.onreadystatechange = () => {
+		if(landmarkRequest.readyState === 4) {
+			if(landmarkRequest.status === 404) {
+				return message.textContent = 'City not found!';
+			}
+			const response = JSON.parse(landmarkRequest.response);
 
-		// var rawResponse = 'PNG'; // This is your response object
-		// var encodedResponse = btoa(response);
+			checkUndefined(response.Response.View[0].Result[0], landmark1Name);
+			checkUndefined(response.Response.View[0].Result[1], landmark2Name);
+			checkUndefined(response.Response.View[0].Result[2], landmark3Name);
+			checkUndefined(response.Response.View[0].Result[3], landmark4Name);
 
-		// var img = new Image();
-		// var container = document.getElementById('newImg');
+		};
+	}
+	landmarkRequest.send();
+}
 
-		// img.src = 'data:image/gif;base64,' + encodedResponse;
-
-		// img.onload = function() {
-		// container.appendChild( img );
-		// };
-
-// 	};
-// }
-
-
-
-
-
+// function for displaying landmarks
+function checkUndefined(landmark, landmarkDisplay){
+	if(landmark == undefined){
+		landmarkDisplay.innerText = 'undefined';
+	} else {
+		landmarkDisplay.innerText = landmark.Location.Name;
+	}
+}
 
