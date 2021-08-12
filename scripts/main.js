f2ef645974558897c374598c3acbba8e2b4082ec
AndreUdotai <ubongudotai@gmail.com>
2021-08-12 09:07:07 +0100

    Made minor modifications

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ const searchInputHomePage		= document.getElementById('searchInputHomePage');
 const mapDisplay				= document.getElementById('mapDisplay');
 
 let cityLabel		= document.getElementById('cityLabel');
+// let countryName	= document.getElementById('countryName');
 let weatherCondtion	= document.getElementById('weatherCondition');
 let postalCode		= document.getElementById('postalCode');
 let temperature		= document.getElementById('temperature');
@@ -23,6 +24,8 @@ let apiRequest 			= new XMLHttpRequest();
 let weatherApiRequest 	= new XMLHttpRequest();
 
 let hereApiBaseUrl	= 'https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&searchtext=';
+// let weatherUrl		= 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e';
+
 
 
 searchFormHomePage.addEventListener('submit', ($event) => {
@@ -38,6 +41,8 @@ searchFormHomePage.addEventListener('submit', ($event) => {
 	weatherApiRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e&units=metric');
 	weatherApiRequest.send();
 
+	
+	// landmarkRetrieve()
 });
 
 
@@ -68,7 +73,9 @@ apiRequest.onreadystatechange = () => {
 
 		buildMap(latitudeCoordinate, longitudeCoordinate);
 
-		landmarkRetrieve(latitudeCoordinate, longitudeCoordinate);
+		let latitueTest 	= 6.4584;
+		let longitudeTest 	= 7.5464;
+		landmarkRetrieve(latitueTest, longitudeTest);
 
 	};
 };
@@ -82,6 +89,7 @@ function buildMap(latitude, longitude){
 		center: 	[latitude, longitude],
 		layers: 	L.mapquest.tileLayer('map'),
 		zoom: 		12,
+		// marker:	Default,
 		type: 		'dark',
 	});
 
@@ -131,17 +139,33 @@ function landmarkRetrieve(lat, long){
 			}
 			const response = JSON.parse(landmarkRequest.response);
 
-			checkUndefined(response.Response.View[0].Result[0], landmark1Name);
-			checkUndefined(response.Response.View[0].Result[1], landmark2Name);
-			checkUndefined(response.Response.View[0].Result[2], landmark3Name);
-			checkUndefined(response.Response.View[0].Result[3], landmark4Name);
+
+			landmark1 	= response.Response.View[0].Result[0].Location.Name;
+			landmark2	= response.Response.View[0].Result[1].Location.Name;
+			// landmark3	= response.Response.View[0].Result[2].Location.Name;
+			// landmark4	= response.Response.View[0].Result[3].Location.Name;
+
+			console.log(response.Response.View[0].Result[2])
+
+	
+			landmark1Name.innerText	= landmark1;
+			landmark2Name.innerText	= landmark2;
+
+			checkUndefined(response.Response.View[0].Result[0], landmark1Name)
+			checkUndefined(response.Response.View[0].Result[1], landmark2Name)
+			checkUndefined(response.Response.View[0].Result[2], landmark3Name)
+			checkUndefined(response.Response.View[0].Result[3], landmark4Name)
+
+			
+
+
+			
 
 		};
 	}
 	landmarkRequest.send();
 }
 
-// function for displaying landmarks
 function checkUndefined(landmark, landmarkDisplay){
 	if(landmark == undefined){
 		landmarkDisplay.innerText = 'undefined';
@@ -149,4 +173,68 @@ function checkUndefined(landmark, landmarkDisplay){
 		landmarkDisplay.innerText = landmark.Location.Name;
 	}
 }
+
+
+
+
+
+
+/* 
+ * *******************************************************
+ * NOTES ON FIXES
+ * *******************************************************
+ * 1. The city name should be emphasized while the state name and country name should not be emphasized.
+ * 2. Where the same name of a city occur in different countries, display a message informaing the client and suggesting the search input to get data for the other cities.
+ * 3. The coordinates information should have appropriate suffixes e.g. '°S'
+ * 4. weather icons should correspond with the weather condition
+ * 5. 
+*/
+
+
+
+
+// const baseUrl 		= 'https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&searchtext=';
+// const city 			= searchInputHomePage.value;
+// const url			= baseUrl + city;
+
+// function makeRequest(){
+// 	return new Promise((resolve, reject) => {
+// 		let request = new XMLHttpRequest();
+// 		request.open('GET', url);
+// 		request.onreadystatechange = () => {
+// 			if(request.readyState === 4){
+// 				if(request.status == 200){
+// 					resolve(JSON.parse(request.response));
+// 				} else {
+// 					reject(JSON.parse(request.response));
+// 				}
+// 			}
+// 		}
+// 		request.send();
+// 	})
+// }
+
+// async function submitFormData(){
+// 	const requestPromise	= makeRequest();
+// 	const response			= await requestPromise;
+
+// 	let longitudeCoordinate = response.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
+// 	let latitudeCoordinate 	= response.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+
+// 	L.mapquest.key = '8Dki0pvPAIdaPvLx8D7svwxs72IUthCN';
+
+// 	var map = L.mapquest.map('map', {
+// 	center: 	[latitudeCoordinate, longitudeCoordinate],
+// 	layers: 	L.mapquest.tileLayer('map'),
+// 	zoom: 		12,
+// 	type: 		'dark',
+// 	});
+
+// 	map.addControl(L.mapquest.control());
+// }
+
+// searchFormHomePage.addEventListener('submit', ($event) => {
+// 	$event.preventDefault();
+// 	submitFormData();
+// });
 
