00e34ff2d743357ca913447e7e8c4dd82188eea1
AndreUdotai <ubongudotai@gmail.com>
2021-08-12 19:57:49 +0100

    made improvements

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,6 @@ const searchInputHomePage		= document.getElementById('searchInputHomePage');
 const mapDisplay				= document.getElementById('mapDisplay');
 
 let cityLabel		= document.getElementById('cityLabel');
-// let countryName	= document.getElementById('countryName');
 let weatherCondtion	= document.getElementById('weatherCondition');
 let postalCode		= document.getElementById('postalCode');
 let temperature		= document.getElementById('temperature');
@@ -24,8 +23,6 @@ let apiRequest 			= new XMLHttpRequest();
 let weatherApiRequest 	= new XMLHttpRequest();
 
 let hereApiBaseUrl	= 'https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&searchtext=';
-// let weatherUrl		= 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e';
-
 
 
 searchFormHomePage.addEventListener('submit', ($event) => {
@@ -41,8 +38,6 @@ searchFormHomePage.addEventListener('submit', ($event) => {
 	weatherApiRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e&units=metric');
 	weatherApiRequest.send();
 
-	
-	// landmarkRetrieve()
 });
 
 
@@ -64,18 +59,20 @@ apiRequest.onreadystatechange = () => {
 		if(postCode != undefined){
 			postalCode.textContent	= 'Postal code: ' + postCode;
 		} else {
-			postalCode.textContent	= 'Not Available.'
+			postalCode.textContent	= 'Postal Code is not available for this city.'
 		}
 		longitude.innerText			= 'Longitude: ' + longitudeCoordinate;
 		latitude.innerText			= 'Latitude: ' + latitudeCoordinate + ' | ';
+
+
+		// (postCode != undefined) ? 'Postal code: ' + poseCode : 'Postal code is not available for this city';
+
 
 		map.remove();
 
 		buildMap(latitudeCoordinate, longitudeCoordinate);
 
-		let latitueTest 	= 6.4584;
-		let longitudeTest 	= 7.5464;
-		landmarkRetrieve(latitueTest, longitudeTest);
+		landmarkRetrieve(latitudeCoordinate, longitudeCoordinate);
 
 	};
 };
@@ -89,7 +86,6 @@ function buildMap(latitude, longitude){
 		center: 	[latitude, longitude],
 		layers: 	L.mapquest.tileLayer('map'),
 		zoom: 		12,
-		// marker:	Default,
 		type: 		'dark',
 	});
 
@@ -139,102 +135,31 @@ function landmarkRetrieve(lat, long){
 			}
 			const response = JSON.parse(landmarkRequest.response);
 
-
-			landmark1 	= response.Response.View[0].Result[0].Location.Name;
-			landmark2	= response.Response.View[0].Result[1].Location.Name;
-			// landmark3	= response.Response.View[0].Result[2].Location.Name;
-			// landmark4	= response.Response.View[0].Result[3].Location.Name;
-
-			console.log(response.Response.View[0].Result[2])
-
-	
-			landmark1Name.innerText	= landmark1;
-			landmark2Name.innerText	= landmark2;
-
-			checkUndefined(response.Response.View[0].Result[0], landmark1Name)
-			checkUndefined(response.Response.View[0].Result[1], landmark2Name)
-			checkUndefined(response.Response.View[0].Result[2], landmark3Name)
-			checkUndefined(response.Response.View[0].Result[3], landmark4Name)
-
-			
+			let landmarkCard1 = document.getElementById('landmarkCard1');
+			let landmarkCard2 = document.getElementById('landmarkCard2');
+			let landmarkCard3 = document.getElementById('landmarkCard3');
+			let landmarkCard4 = document.getElementById('landmarkCard4');
 
 
-			
+			checkUndefined(response.Response.View[0].Result[0], landmark1Name, landmarkCard1);
+			checkUndefined(response.Response.View[0].Result[1], landmark2Name, landmarkCard2);
+			checkUndefined(response.Response.View[0].Result[2], landmark3Name, landmarkCard3);
+			checkUndefined(response.Response.View[0].Result[3], landmark4Name, landmarkCard4);
 
 		};
 	}
 	landmarkRequest.send();
 }
 
-function checkUndefined(landmark, landmarkDisplay){
-	if(landmark == undefined){
-		landmarkDisplay.innerText = 'undefined';
-	} else {
-		landmarkDisplay.innerText = landmark.Location.Name;
-	}
+// function for displaying landmarks
+let checkUndefined = (l,ln, lc) => {
+	if(l != undefined)
+		lc.style.display	= 'inline';
+		ln.innerText		= l.Location.Name;
 }
 
 
-
-
-
-
-/* 
- * *******************************************************
- * NOTES ON FIXES
- * *******************************************************
- * 1. The city name should be emphasized while the state name and country name should not be emphasized.
- * 2. Where the same name of a city occur in different countries, display a message informaing the client and suggesting the search input to get data for the other cities.
- * 3. The coordinates information should have appropriate suffixes e.g. '°S'
- * 4. weather icons should correspond with the weather condition
- * 5. 
-*/
-
-
-
-
-// const baseUrl 		= 'https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&searchtext=';
-// const city 			= searchInputHomePage.value;
-// const url			= baseUrl + city;
-
-// function makeRequest(){
-// 	return new Promise((resolve, reject) => {
-// 		let request = new XMLHttpRequest();
-// 		request.open('GET', url);
-// 		request.onreadystatechange = () => {
-// 			if(request.readyState === 4){
-// 				if(request.status == 200){
-// 					resolve(JSON.parse(request.response));
-// 				} else {
-// 					reject(JSON.parse(request.response));
-// 				}
-// 			}
-// 		}
-// 		request.send();
-// 	})
+// // Using Tenary operator
+// let checkUndefined = (l, ln, ld) => {
+// 	l != undefined ? lc.style.display = 'inline' : 
 // }
-
-// async function submitFormData(){
-// 	const requestPromise	= makeRequest();
-// 	const response			= await requestPromise;
-
-// 	let longitudeCoordinate = response.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
-// 	let latitudeCoordinate 	= response.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
-
-// 	L.mapquest.key = '8Dki0pvPAIdaPvLx8D7svwxs72IUthCN';
-
-// 	var map = L.mapquest.map('map', {
-// 	center: 	[latitudeCoordinate, longitudeCoordinate],
-// 	layers: 	L.mapquest.tileLayer('map'),
-// 	zoom: 		12,
-// 	type: 		'dark',
-// 	});
-
-// 	map.addControl(L.mapquest.control());
-// }
-
-// searchFormHomePage.addEventListener('submit', ($event) => {
-// 	$event.preventDefault();
-// 	submitFormData();
-// });
-
