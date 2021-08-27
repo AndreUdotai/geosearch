1de4d7955775cdb6acb0c17dd8a075f833ac336c
AndreUdotai <ubongudotai@gmail.com>
2021-08-27 03:56:52 +0100

    made updates

--- scripts/main.js
+++ scripts/main.js
@@ -3,79 +3,70 @@ const searchFormHomePage 		= document.getElementById('searchFormHomePage');
 const searchInputLandingPage	= document.getElementById('searchInputLandingPage');
 const searchInputHomePage		= document.getElementById('searchInputHomePage');
 const mapDisplay				= document.getElementById('mapDisplay');
-
-let cityLabel		= document.getElementById('cityLabel');
-let weatherCondtion	= document.getElementById('weatherCondition');
-let postalCode		= document.getElementById('postalCode');
-let temperature		= document.getElementById('temperature');
-let humidity		= document.getElementById('humidity');
-let windSpeed		= document.getElementById('windSpeed');
-let longitude		= document.getElementById('longitude');
-let latitude		= document.getElementById('latitude');
-let celsius			= document.getElementById('celsius');
-let fahrenheit		= document.getElementById('fahrenheit');
-let landmarkName 	= document.getElementById('landmarkName');
-let temperatureControls = document.getElementById('temperatureControls');
-
-
-
-let apiRequest 			= new XMLHttpRequest();
-let weatherApiRequest 	= new XMLHttpRequest();
-
-let hereApiBaseUrl	= 'https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&searchtext=';
-
+const clearButton				= document.getElementById('clearButton');
+const landmarks 				= document.getElementById('landmarks');
+const mapContainer				= document.getElementById('mapContainer');
+const alert						= document.getElementById('alert');
 
 searchFormHomePage.addEventListener('submit', ($event) => {
 	$event.preventDefault();
-
-	temperatureControls.style.display = 'inline';
-
-	const chosenCity = searchInputHomePage.value;
-	const url = hereApiBaseUrl + chosenCity + '&mode=retrieveLandmarks&prox=6.4584,7.5464,1000';
-	apiRequest.open('GET', url);
-	apiRequest.send();
-
-	weatherApiRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e&units=metric');
-	weatherApiRequest.send();
-
+	//Calling the requestHereAPIInfo function
+	requestHereAPIInfo()
+	//Calling the requestWeatherAPIInfo function
+	requestWeatherAPIInfo()
+});
+searchFormHomePage.addEventListener('input', () => {
+	clearButton.innerText = '×';
+})
+clearButton.addEventListener('click', () => {
+	searchFormHomePage.reset();
+	clearButton.innerText = ' ';
 });
 
+function alertError(alertMessage){
+	let alert = document.createElement('div');
+	alert.innerHTML = `<div class="alert alert-warning alert-dismissible fade show mb-1 mt-1" role="alert">
+	${alertMessage}
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+	  <span aria-hidden="true">&times;</span>
+	</button>
+  </div>`
+	mapContainer.prepend(alert);
+}
 
-apiRequest.onreadystatechange = () => {
-	if(apiRequest.readyState === 4) {
-		if(apiRequest.status === 404) {
-			return message.textContent = 'City not found!';
-		}
+// // This function queries the Here API to extract location details for the searched city
+function requestHereAPIInfo(){
+	const chosenCity	= searchInputHomePage.value;
+	const url 			= 'https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&searchtext=' + chosenCity + '&mode=retrieveLandmarks&prox=6.4584,7.5464,1000';
 
-		const response = JSON.parse(apiRequest.response);
+	fetch(url)
+		.then(response => {
+			if (!response.ok) {
+				throw Error(response.statusText);
+			}
+			return response 
+	  	})
+		.then(response => response.json())
+		.then(data => {
+			let outcome = data;
+			let longitudeCoordinate 	= outcome.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
+			let latitudeCoordinate 		= outcome.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+			let postCode				= outcome.Response.View[0].Result[0].Location.Address.PostalCode;
+			let cityName				= outcome.Response.View[0].Result[0].Location.Address.Label;
 
-		let longitudeCoordinate 	= response.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
-		let latitudeCoordinate 		= response.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+			document.getElementById('cityInfo2').innerHTML 	= 	`<div class="card border-white">
+			<div class="card-body" id="cityInfo"><span class="h4">${cityName}</span> <br>
+				Latitude: ${latitudeCoordinate} | Longitude: ${longitudeCoordinate} <br>
+				${postCode != undefined ? 'Postal code: ' + postCode : 'Postal Code is not available for this city.'}</div></div>`;
 
-		cityLabel.innerText 		= response.Response.View[0].Result[0].Location.Address.Label;
-		let postCode				= response.Response.View[0].Result[0].Location.Address.PostalCode;
-
-
-		if(postCode != undefined){
-			postalCode.textContent	= 'Postal code: ' + postCode;
-		} else {
-			postalCode.textContent	= 'Postal Code is not available for this city.'
-		}
-		longitude.innerText			= 'Longitude: ' + longitudeCoordinate;
-		latitude.innerText			= 'Latitude: ' + latitudeCoordinate + ' | ';
-
-
-		// (postCode != undefined) ? 'Postal code: ' + poseCode : 'Postal code is not available for this city';
-
-
-		map.remove();
-
-		buildMap(latitudeCoordinate, longitudeCoordinate);
-
-		landmarkRetrieve(latitudeCoordinate, longitudeCoordinate);
-
-	};
-};
+			// This method clears a previously searched map for onward display of another map from a new query
+			map.remove();
+			// Calling the builMap function and passing in the extracted coordinates from the searched city as arguments.
+			buildMap(latitudeCoordinate, longitudeCoordinate);
+			// Calling the landmarkRetrieve function and passing in the extracted coordinates as arguments
+			landmarkRetrieve(latitudeCoordinate, longitudeCoordinate);
+		})
+}
 
 // function for building the mapquest web SDK
 function buildMap(latitude, longitude){
@@ -88,8 +79,12 @@ function buildMap(latitude, longitude){
 		zoom: 		12,
 		type: 		'dark',
 	});
-
 	map.addControl(L.mapquest.control());
+
+	L.marker([latitude, longitude],{
+		icon: L.mapquest.icons.marker(),
+		draggable: false
+	}).bindPopup('Denver, CO').addTo(map);
 }
 
 // function for temperature conversion between celsius and fahrenheit
@@ -99,67 +94,90 @@ function convertToF(celsius) {
 	return fahrenheit.toFixed(2);
 }
 
-weatherApiRequest.onreadystatechange = () => {
-	if(weatherApiRequest.readyState === 4) {
-		if(weatherApiRequest.status === 404) {
-			return message.textContent = 'City not found!';
-		}
-		const response = JSON.parse(weatherApiRequest.response);
-
-                            
-		weatherCondtion.innerHTML	= '<span><i class="fa fa-cloud fa-3x" aria-hidden="true"></i> ' + response.weather[0].description  + '</span>';
-		temperature.innerHTML		= '<span>' + response.main.temp +  '</span>';
-		humidity.textContent		= 'Humidity: ' + response.main.humidity + '%';
-		windSpeed.textContent		= 'Wind: ' + response.wind.speed + 'm/s';
-
-		fahrenheit.addEventListener('click', ($event) => {
-			$event.preventDefault();
-			temperature.textContent = convertToF(response.main.temp);
-		})
-
-		celsius.addEventListener('click', ($event) => {
-			$event.preventDefault();
-			temperature.textContent = response.main.temp;
-		})
-	};
-}
-
-function landmarkRetrieve(lat, long){
-	let landmarkRequest		= new XMLHttpRequest();
-	landmarkRequest.open('GET', 'https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&mode=retrieveLandmarks&prox=' + lat + ',' + long + ',1000');
-
-	landmarkRequest.onreadystatechange = () => {
-		if(landmarkRequest.readyState === 4) {
-			if(landmarkRequest.status === 404) {
-				return message.textContent = 'City not found!';
+// //This function queries the Open Weather Map API to extract weather info for the searched location
+function requestWeatherAPIInfo(){
+	const chosenCity = searchInputHomePage.value;
+	fetch('https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e&units=metric')
+		.then(response => {
+			if (!response.ok) { 
+				if(response.statusText == 'Bad Request'){
+					alertError('Ensure to make an input in the search bar. You can search for Street Addresses. Eg "Lagos St, Enugu"')
+				} else if(response.statusText == 'Not Found'){
+					alertError('City not found. Please confirm your spelling and ensure to insert spaces where appropriate. E.g. "Port Harcourt, Rivers"')
+				}
+				throw Error(response.statusText)
 			}
-			const response = JSON.parse(landmarkRequest.response);
+			return response
+	  	})
+		.then(response => response.json())
+		.then(data => {
+			let response = data;
 
-			let landmarkCard1 = document.getElementById('landmarkCard1');
-			let landmarkCard2 = document.getElementById('landmarkCard2');
-			let landmarkCard3 = document.getElementById('landmarkCard3');
-			let landmarkCard4 = document.getElementById('landmarkCard4');
-
-
-			checkUndefined(response.Response.View[0].Result[0], landmark1Name, landmarkCard1);
-			checkUndefined(response.Response.View[0].Result[1], landmark2Name, landmarkCard2);
-			checkUndefined(response.Response.View[0].Result[2], landmark3Name, landmarkCard3);
-			checkUndefined(response.Response.View[0].Result[3], landmark4Name, landmarkCard4);
-
-		};
-	}
-	landmarkRequest.send();
+			document.getElementById('weatherInfo2').innerHTML 	= `<div class="card border-white">
+			<div class="card-body" id="weatherInfo"><i class="fa fa-cloud fa-3x" aria-hidden="true"></i> ${response.weather[0].description} <br>
+										<span class="h1" id='temperature'>${response.main.temp}</span> <sup><span class="h6"><a href="#" id="celsius">°C</a> | <a href="#" id="fahrenheit">°F</a></span></sup> <br>
+										Humidity: ${response.main.humidity}% <br>
+										Wind: ${response.wind.speed}m/s</div></div>`
+			
+			// Remove the landing page
+			document.getElementById('landingPage').remove();
+			// set
+			alertError('Not the location you searched for? Add other parameters like "State" or "Country" names.')
+			
+			document.getElementById('fahrenheit').addEventListener('click', ($event) => {
+				$event.preventDefault();
+				document.getElementById('temperature').textContent = convertToF(response.main.temp);
+			})
+			document.getElementById('celsius').addEventListener('click', ($event) => {
+				$event.preventDefault();
+				document.getElementById('temperature').textContent = response.main.temp;
+			})
+	})
 }
 
-// function for displaying landmarks
-let checkUndefined = (l,ln, lc) => {
-	if(l != undefined)
-		lc.style.display	= 'inline';
-		ln.innerText		= l.Location.Name;
+// This function retrieves the landmarks within 1000m around any coordinates
+function landmarkRetrieve(lat, long){
+	fetch('https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=6C8nSRy83mBnAlo_-OdMnZf9BL1nWaw7UZcZhzk0TAc&mode=retrieveLandmarks&prox=' + lat + ',' + long + ',1000')
+		.then(response => {
+			if (!response.ok) { throw Error(response.statusText) }
+			return response
+	  	})
+		.then(response 	=> response.json())
+		.then(data 		=> {
+			let response = data
+	
+			if(response.Response.View[0] == undefined){
+				landmarks.style.display = 'none';
+			} else {
+				landmarks.style.display = 'flex';
+				while (landmarks.firstChild) {
+					landmarks.removeChild(landmarks.firstChild);
+				}
+				for(let i = 0; i < 4; i++){
+					if(response.Response.View[0].Result[i] != undefined){
+						let column 		= document.createElement('div');
+						let card		= document.createElement('div');
+						let cardBody	= document.createElement('div');
+						let cardText	= document.createElement('p');
+						let cardImage	= document.createElement('img');
+
+						column.classList.add('col-12', 'col-md-6', 'col-lg-3');
+						card.classList.add('card', 'my-2');
+						cardBody.classList.add('card-body');
+						cardText.classList.add('card-text');
+						cardImage.setAttribute('src', 'images/landmark1.jpeg');
+						cardImage.classList.add('card-img-top');
+						cardImage.setAttribute('height', '200');
+
+						column.appendChild(card);
+						card.appendChild(cardImage);
+						card.appendChild(cardBody);
+						cardBody.appendChild(cardText);
+
+						cardText.innerText = response.Response.View[0].Result[i].Location.Name;
+						landmarks.appendChild(column);
+					}
+				}
+			}
+	})
 }
-
-
-// // Using Tenary operator
-// let checkUndefined = (l, ln, ld) => {
-// 	l != undefined ? lc.style.display = 'inline' : 
-// }
